33aafb61eb5da36c85214b1cca854e3923057bb6
Adam <54454429+AdamJD01@users.noreply.github.com>
2020-02-18 22:06:39 +0000

    Initial Commit

---
+++ SleepApp.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace SleepApp
+{
+    partial class SleepApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.numAmount = new System.Windows.Forms.NumericUpDown();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numAmount)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sleep App";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(366, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter the amount of sleep you had last night:";
+            this.label2.UseWaitCursor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(136, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(202, 69);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.UseWaitCursor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.numAmount);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(18, 114);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(453, 53);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.UseWaitCursor = true;
+            // 
+            // numAmount
+            // 
+            this.numAmount.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numAmount.Location = new System.Drawing.Point(373, 21);
+            this.numAmount.Name = "numAmount";
+            this.numAmount.Size = new System.Drawing.Size(61, 26);
+            this.numAmount.TabIndex = 9;
+            this.numAmount.UseWaitCursor = true;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(15, 19);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(117, 55);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.UseWaitCursor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(180, 315);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(105, 51);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 22);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Results";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.UseWaitCursor = true;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Enabled = false;
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(14, 25);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(342, 135);
+            this.txtOutput.TabIndex = 12;
+            this.txtOutput.UseWaitCursor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnCalculate);
+            this.groupBox4.Location = new System.Drawing.Point(162, 194);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(146, 87);
+            this.groupBox4.TabIndex = 13;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.UseWaitCursor = true;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.txtOutput);
+            this.groupBox5.Location = new System.Drawing.Point(52, 372);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(375, 177);
+            this.groupBox5.TabIndex = 14;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.UseWaitCursor = true;
+            // 
+            // SleepApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 566);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox5);
+            this.Name = "SleepApp";
+            this.Text = "SleepApp";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numAmount)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown numAmount;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+    }
+}
---
+++ SleepApp.cs
@@ -0,0 +1,81 @@
+﻿//Written by: AdamJD
+//Date: 19/10/2019 
+//Description: An app that calculates how much sleep the user needs to stay healthy based on the amount of sleep they had the night before.
+
+using System;
+using System.Windows.Forms;
+
+namespace SleepApp
+{
+    public partial class SleepApp : Form
+    {
+        //variables
+        private int idealAmount = 8; //the ideal hours of sleep
+        private float idealAmountPlusOne; //for the recommended range
+        private float idealAmountMinusOne; //for the recommended range
+        private float outputAmount; //hours of sleep away from ideal amount
+        private string finalOutput; //message that is displayed
+
+        public SleepApp()
+        {
+            InitializeComponent(); //load up app
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            float inputAmount; //get the inputted sleep amount
+            inputAmount = float.Parse(numAmount.Text); //parses the input as a float, the NumericUpDown counts minus numbers as 0, rounds up from .5, stops user entering text, leaving this blank and ignores whitespace
+
+            //set up ranges for the recommended range
+            idealAmountPlusOne = idealAmount + 1; //8 + 1
+            idealAmountMinusOne = idealAmount - 1; //8 - 1
+
+            if (inputAmount == idealAmount) //8 hours sleep
+            {
+                finalOutput = ("You have had " + idealAmount + " hour's of sleep! Congratulations, keep it up!"); //got 8 hours sleep message
+                txtOutput.Text = finalOutput.ToString(); //show message to user
+            }
+
+            else if ((inputAmount < idealAmount && inputAmount >= idealAmountMinusOne) || (inputAmount > idealAmount && inputAmount <= idealAmountPlusOne)) //between 7-9 hours sleep
+            {
+                outputAmount = idealAmount - inputAmount; //calculate how far away the user is from 8 hours sleep
+                finalOutput = ("You have had " + inputAmount + " hour/s of sleep which is " + outputAmount + " hour/'s of sleep away from the magic 8 hours. This is within the recommended range."); //got 7 hours sleep message
+                txtOutput.Text = finalOutput.ToString(); //show message to user
+
+                if (outputAmount < 0)
+                {
+                    outputAmount *= -1; //get rid of minus sign if output amount is below 0
+                    finalOutput = ("You have had " + inputAmount + " hour/s of sleep which is " + outputAmount + " hour/'s of sleep over the magic 8 hours. This is within the recommended range."); //got 9 hours sleep message
+                    txtOutput.Text = finalOutput.ToString(); //show message to user
+                }
+            }
+
+            else if (inputAmount < idealAmountMinusOne && inputAmount >= 0) //0-6 hours of sleep 
+            {
+                outputAmount = idealAmount - inputAmount; //calculate how far away the user is from 8 hours sleep
+                finalOutput = ("You have had " + inputAmount + " hour/s of sleep which is " + outputAmount + " hour/'s sleep under the recommended range. Get some more sleep to be in the healthy 7-9 hour range!"); //got under 7 hours sleep message
+                txtOutput.Text = finalOutput.ToString(); //show message to user
+
+                if (inputAmount >= 0 && inputAmount <= 3) //0-3 hours sleep
+                {
+                    outputAmount = idealAmount - inputAmount; //calculate how far away the user is from 8 hours sleep
+                    finalOutput = ("You have had " + inputAmount + " hour/s of sleep which is " + outputAmount + " hour/'s sleep under the recommended range. What the fuck were you doing to get this amount of sleep? Go to bed!"); //tell the user to go to bed!
+                    txtOutput.Text = finalOutput.ToString(); //show message to user
+                }
+            }
+
+            else if (inputAmount > idealAmountPlusOne && inputAmount <= 24) //10-24 hours of sleep (must have been hell of a party if this is the amount...)
+            {
+                outputAmount = inputAmount - idealAmount; //calculate how far over the user is from 8 hours sleep
+                finalOutput = ("You have had " + inputAmount + " hour/s of sleep which is " + outputAmount + " hour/'s of sleep over the recommended range. More than 7-9 hours sleep is bad for your health!"); //got over 9 hours sleep message
+                txtOutput.Text = finalOutput.ToString(); //show message to user
+            }
+
+            else //validation
+            {
+                finalOutput = "Please enter a valid number between 0 and 24";  //enter correct value message
+                txtOutput.Text = finalOutput.ToString(); //show message to user
+            }
+        }
+    }
+}
